59d3b92d8c7c3b08ae3b14e38af8378ce8f0c468
Amirreza <bzamr@outlook.com>
2024-07-31 14:35:06 +0330

    feat: add rules modal

--- src/main.js
+++ src/main.js
@@ -1,19 +1,14 @@
 import '../styles/style.scss'
 import { change_board_to } from './board';
 import { footer } from './footer';
-import { choice_menu } from './machine-ai';
 import score_bar from './score-bar';
-import update_ui from './update-ui';
-
 
 document.querySelector('#app').innerHTML = 
     `<div class="score-bar"></div>
       <div class="board"></div>
       <div class="footer"></div>`;
 
-
 //Init 
-
 score_bar(document.querySelector(".score-bar"),(localStorage.getItem("score") | 0));
 change_board_to().triple_options(document.querySelector(".board"));
 footer(document.querySelector(".footer"));
