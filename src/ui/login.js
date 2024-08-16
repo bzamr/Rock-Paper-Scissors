cbdde3930b09cbab9d621373f9216650a01259c3
Amirreza <bzamr@outlook.com>
2024-08-16 20:05:13 +0330

    refactor(auth ui): move forms logical block to another file and simplify markup file

---
+++ src/ui/login.js
@@ -0,0 +1,14 @@
+export default function loginPage() {
+  return `
+        <div class="login-page">
+            <button id="goHome">&#129136;</button>
+            <h1>LOGIN</h1>
+            <p>Welcome back! Let's take you to your account.</p>
+            <form>
+            <input type="email" id="email" name="email" placeholder="Email Address">
+            <input type="password" id="password" name="password" placeholder="Password">
+            </form>
+        <button id="loginButton">continue</button>
+        </div>
+    `;
+}
